d5a144216d2e7963e8313dfdaddbe24e1c09c6d0
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-03-07 13:54:21 +0000

    fix: medusa.config

--- medusa-config.ts
+++ medusa-config.ts
@@ -16,7 +16,8 @@ module.exports = defineConfig({
     redisUrl: process.env.REDIS_URL,
   },
   admin: {
-    backendUrl: process.env.MEDUSA_BACKEND_URL,
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    // backendUrl: process.env.MEDUSA_BACKEND_URL,
   },
   modules: [
     {
