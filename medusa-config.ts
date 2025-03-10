bfbb80341cd6d587a65265aaa9232180c216db75
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-03-10 18:21:04 +0000

    feat: use stripe plugin

--- medusa-config.ts
+++ medusa-config.ts
@@ -62,12 +62,13 @@ module.exports = defineConfig({
         ],
       },
     },
+  ],
+  plugins: [
     {
       resolve: "medusa-payment-stripe",
       options: {
         api_key: process.env.STRIPE_API_KEY,
         webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-        automatic_payment_methods: true,
       },
     },
   ],
